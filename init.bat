597a8028e05cfb3d2867b98caeef0cf003a9d66c
Ian Knowles <ian@ciyam.com>
2012-01-18 16:32:00 +0800

    cont'd...

--- init.bat
+++ init.bat
@@ -3,12 +3,14 @@ setlocal
 if '%1' == '' goto usage
 
 del %1.modules.lst %1.log %1.dat %1.idx %1.hdr >nul
-set CAT_APPLICATION=%1
+set CIYAM_MODEL=%1
+set CIYAM_STORAGE=%1
+if not '%2' == '' set CIYAM_STORAGE=%2
 cat_client -quiet -no_prompt < init.cin
 goto end
 
 :usage
-echo Usage: init [app name]
+echo Usage: init [model name] [[app name]]
 
 :end
 endlocal
