d233fa60dcb60b8b17e7612d6175c928c7dfe2cd
Ian Knowles <ian@ciyam.com>
2012-03-14 09:00:20 +0800

    cont'd...

--- config.h
+++ config.h
@@ -28,7 +28,7 @@ const int c_protocol_minor_version = 1;
 
 const char* const c_protocol_version = "0.1";
 
-const size_t c_password_hash_rounds = 10000;
+const size_t c_password_hash_rounds = 1000;
 
 #endif
 
---
+++ genfcgi.bat
@@ -0,0 +1,42 @@
+@echo off
+setlocal
+
+if '%1' == '' goto usage
+if '%2' == '' goto usage
+
+if '%3' == '' goto skip
+set WEBDIR=%3
+
+:skip
+if "%WEBDIR%" == "" goto error1
+if not exist %1.app.vars.xrep goto error2
+
+if not exist %WEBDIR%\%2 call setup.bat %1 %2 %3
+
+if not exist %WEBDIR%\%2\activate.htms goto error3
+
+xrep @app.fcgi.sio.xrep include=%1.app.vars.xrep >~genfcgi.tmp
+call update.bat "%WEBDIR%\%2\fcgi.sio" ~genfcgi.tmp
+
+xrep @ciyam_interface.js.xrep include=%1.app.vars.xrep >~genfcgi.tmp
+call update.bat "%WEBDIR%\%2\ciyam_interface.js" ~genfcgi.tmp
+goto end
+
+:error1
+echo Error: Missing environment variable 'WEBDIR'.
+goto end
+
+:error2
+echo Error: File '%1.app.vars.xrep' not found.
+goto end
+
+:error3
+echo Error: File '%WEBDIR%\%2\activate.htms' not found.
+goto end
+
+:usage
+echo Usage: genfcgi [app name] [app dir] [[web dir]]
+
+:end
+endlocal
+
