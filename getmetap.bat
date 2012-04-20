62eef8d32197412d0eb32c830810893094ac27ec
Ian Knowles <ian@ciyam.com>
2012-04-20 15:25:23 +0800

    Added getmetap batch file.

---
+++ getmetap.bat
@@ -0,0 +1,41 @@
+@echo off
+setlocal
+
+if '%1' == '' goto usage
+if '%2' == '' goto usage
+if '%3' == '' goto usage
+
+if '%CIYAM_PACKAGE%' == '' goto error1
+
+set mtype=%1
+set keyval=%2
+set fields=%3
+
+:loop
+if '%4' == '' goto next
+set fields=%fields%,%4
+shift
+goto loop
+
+:next
+call getmeta.bat %mtype% %keyval% "%fields%" %CIYAM_PACKAGE% >~getmetap
+
+VER | FINDSTR /IL "5.1." > NUL
+IF %ERRORLEVEL% EQU 0 clipset @~getmetap
+
+VER | FINDSTR /IL "6." > NUL
+IF %ERRORLEVEL% EQU 0 type ~getmetap | clip
+
+del ~getmetap
+echo (result is on the clipboard)
+goto end
+
+:error1
+echo Error: Missing environment variable 'CIYAM_PACKAGE'.
+goto end
+
+:usage
+echo Usage: getmetap [meta type] [key val] [field1,[field2,[...]]]
+
+:end
+
