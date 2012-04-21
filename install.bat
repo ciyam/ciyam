ddb9b750b5c17158cc87aa36ddd4e9ab13cc16ac
Ian Knowles <ian@ciyam.com>
2012-04-21 11:59:44 +0800

    Added install batch and getting started document.

---
+++ install.bat
@@ -0,0 +1,36 @@
+@echo off
+setlocal
+
+if '%1' == '' goto usage
+if '%2' == '' goto usage
+
+set dest=%2
+set alias=%3
+
+if not exist %1.fcgi.sio goto error1
+if not exist %WEBDIR%\%dest%\fcgi.sio goto error2
+
+if not exist "%WEBDIR%\%dest%\%1.jpg" copy module.jpg "%WEBDIR%\%dest%\%1.jpg" >nul
+
+copy %1.fcgi.sio "%WEBDIR%\%dest%" >nul
+
+if '%alias%' == '' goto next
+xrep @module.html.xrep model_vars=%1.vars.xrep > %WEBDIR%\%dest%\%alias%.html
+
+:next
+goto end
+
+:error1
+echo Error: File '%1.fcgi.sio' does not exist.
+goto end
+
+:error2
+echo Error: File '%WEBDIR%\%dest%\fcgi.sio' does not exist.
+goto end
+
+:usage
+echo Usage: install [module name] [app dir] [[module alias]]
+
+:end
+endlocal
+
