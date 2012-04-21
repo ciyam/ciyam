942a4f8260ab0aff91cbf763a96303e02e910b3d
Ian Knowles <ian@ciyam.com>
2012-04-21 12:42:34 +0800

    Added app and module create/remove batch files.

---
+++ remove_fcgi.bat
@@ -0,0 +1,35 @@
+@echo off
+
+if '%1' == '' goto usage
+
+if '%2' == '' goto skip
+set WEBDIR=%2
+
+:skip
+if "%WEBDIR%" == "" goto error1
+if not exist "%WEBDIR%\%1\fcgi.sio" goto error2
+
+if exist "%WEBDIR%\%1\upload.kill.bat" del "%WEBDIR%\%1\upload.kill.bat"
+if exist "%WEBDIR%\%1\cat_interface.kill.bat" del "%WEBDIR%\%1\cat_interface.kill.bat"
+
+#REM Use PING to approximate a 2 second pause...
+PING 127.0.0.1 -n 3 >nul
+
+if exist "%WEBDIR%\%1\upload.kill.bat" call "%WEBDIR%\%1\upload.kill.bat"
+if exist "%WEBDIR%\%1\cat_interface.kill.bat" call "%WEBDIR%\%1\cat_interface.kill.bat"
+rmdir /s /q "%WEBDIR%\%1"
+goto end
+
+:error1
+echo Error: Missing environment variable 'WEBDIR'.
+goto end
+
+:error1
+echo Error: File '%WEBDIR%\%1\fcgi.sio' does not exist.
+goto end
+
+:usage
+echo Usage: remove_fcgi [app dir] [[web dir]]
+
+:end
+
---
+++ remove_module.bat
@@ -0,0 +1,62 @@
+@echo off
+setlocal
+
+set is_rdbms=
+
+if not '%1' == '-rdbms' goto next
+set is_rdbms=1
+shift
+
+:next
+if '%1' == '' goto usage
+
+if '%is_rdbms%' == '1' goto skip_check
+if not exist %1.cpp goto error1
+
+:skip_check
+xrep @remove_module.xrep name=%1 modules=@modules.lst >modules.lst.new
+if errorlevel 1 goto end
+
+call update.bat modules.lst modules.lst.new
+call genmake.bat
+
+if not '%is_rdbms%' == '1' goto skip_file_remove
+xrep @remove_module_classes.xrep module=%1 all_classes=@%1.classes.lst >~remove_module.bat
+call ~remove_module.bat
+del ~remove_module.bat
+
+if exist %1.cms del %1.cms
+if exist %1.cmh del %1.cmh
+if exist %1.cpp del %1.cpp
+if exist %1.obj del %1.obj
+if exist %1.lib del %1.lib
+if exist %1.dll del %1.dll
+if exist %1.exp del %1.exp
+if exist %1.dep del %1.dep
+if exist %1.inc del %1.inc
+if exist %1.txt del %1.txt
+if exist %1.sql del %1.sql
+if exist %1.init.lst del %1.init.lst
+if exist %1.fcgi.sio del %1.fcgi.sio
+if exist %1.vars.xrep del %1.vars.xrep
+if exist %1.classes.lst del %1.classes.lst
+if exist %1.acyclic.lst del %1.acyclic.lst
+if exist %1.make.vars.xrep del %1.make.vars.xrep
+if exist %1.spec.vars.xrep del %1.spec.vars.xrep
+if exist %1.spec.vars.xrep.sav del %1.spec.vars.xrep.sav
+goto end
+
+:skip_file_remove
+echo.
+echo *** Module has been removed (it should now be safe to delete all applicable '%1*' files).
+goto end
+
+:error1
+echo Error: Module '%1' does not exist.
+goto end
+
+:usage
+echo Usage: remove_module [[-rdbms]] [module_name]
+
+:end
+
