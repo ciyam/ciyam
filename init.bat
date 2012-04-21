81092cf845a66849fd5bbfde705bb73d4266d677
Ian Knowles <ian@ciyam.com>
2012-04-21 10:34:24 +0800

    Minor changes for setting up Meta system.

--- init.bat
+++ init.bat
@@ -6,8 +6,8 @@ set CIYAM_MODEL=%1
 set CIYAM_STORAGE=%1
 if not '%2' == '' set CIYAM_STORAGE=%2
 del %CIYAM_STORAGE%.log %CIYAM_STORAGE%.dat %CIYAM_STORAGE%.idx %CIYAM_STORAGE%.hdr >nul
-ping 127.0.0.1 -n 2 > nul
 start /min ciyam_server -quiet
+ping 127.0.0.1 -n 2 > nul
 ciyam_client -quiet -no_prompt < init.cin
 touch ciyam_server.stop
 goto end
