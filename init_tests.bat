3dfe1ab79d51edbad8451e78a09a2d1592b4dbbe
Ian Knowles <ian@ciyam.com>
2012-04-22 14:03:22 +0800

    Minor source code formatting changes.

--- init_tests.bat
+++ init_tests.bat
@@ -3,5 +3,6 @@
 if not exist packages.lst echo Standard>packages.lst
 call construct @packages.lst ciyam_class.cpp.xrep
 
-if not exist .git/hooks/pre-commit copy pre-commit .git/hooks >nul
+if exist .git\hooks\pre-commit del .git\hooks\pre-commit
+if exist pre-commit copy pre-commit .git\hooks >nul
 
