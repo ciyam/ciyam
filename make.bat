4efd286511b75b72296d3c75e1753647d6865c99
Ian Knowles <ian@ciyam.com>
2012-05-03 12:44:09 +0800

    Reworked make for debug builds (Windows only).

--- make.bat
+++ make.bat
@@ -7,13 +7,13 @@ if '%CPPENV%' == 'MVC' goto mvc
 goto error1
 
 :bcb
-make.exe -fmakefile.bcb %* | findstr /V "MAKE Version"
+make.exe -fmakefile.bcb %CPPDBG% %* | findstr /V "MAKE Version"
 goto end
 
 :mvc
 if not exist gendeps.exe call cl.bat gendeps.cpp
 nmake.exe /nologo /f makefile.mvc NO_INC_FILES=1 touch_incs
-nmake.exe /nologo /f makefile.mvc %*
+nmake.exe /nologo /f makefile.mvc %CPPDBG% %*
 goto end
 
 :error1
