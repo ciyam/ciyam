0cee6ec7430d29887a2b96e73ce9fe9c61d891b0
Ian Knowles <ian@ciyam.com>
2012-05-30 14:23:38 +0800

    Changed ciyam_server.sio to be a .default implementation.

--- config.bat
+++ config.bat
@@ -1,6 +1,7 @@
 @echo off
 
 if not exist config.info copy config.info.default config.info
+if not exist ciyam_server.sio copy ciyam_server.sio.default ciyam_server.sio
 
 xrep @config.h.xrep >config.h.new
 call update.bat config.h config.h.new
