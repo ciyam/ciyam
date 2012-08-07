7b7ea747da4033eabfaf3f352e673f4288b4cf16
Ian Knowles <ian@ciyam.com>
2012-08-07 11:00:48 +0800

    Added bootstrap makefiles for initial configuration.

--- config.bat
+++ config.bat
@@ -3,6 +3,12 @@
 if not exist config.info copy config.info.default config.info
 if not exist ciyam_server.sio copy ciyam_server.sio.default ciyam_server.sio
 
+if exist xrep.exe goto has_tools
+if not exist makefile.bcb copy makefile.bcb.tools makefile.bcb
+if not exist makefile.mvc copy makefile.mvc.tools makefile.mvc
+call make.bat xrep xvars
+
+:has_tools
 xrep @config.h.xrep >config.h.new
 call update.bat config.h config.h.new
 
