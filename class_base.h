d96785a53a7d165c4abcc843696877465a85b1e1
Ian Knowles <ian@ciyam.com>
2012-09-29 17:43:16 +0800

    Fixed template instantiation issue affecting Linux build.

--- class_base.h
+++ class_base.h
@@ -837,14 +837,14 @@ bool CLASS_BASE_DECL_SPEC is_valid_date_time( const std::string& s );
 bool CLASS_BASE_DECL_SPEC is_valid_value( const std::string& s, primitive p,
  unsigned int max_size = 0, const char* p_min_value = 0, const char* p_max_value = 0 );
 
+template< typename T > inline std::string to_rep_string( const T& t ) { return to_string( t ); }
+
 inline std::string to_string( const class_base& cb ) { return cb.get_key( ); }
 
 inline bool operator ==( const class_base& cb, const std::string& s ) { return s == to_string( cb ); }
 inline bool operator !=( const class_base& cb, const std::string& s ) { return s != to_string( cb ); }
 
 void CLASS_BASE_DECL_SPEC from_string( class_base& cb, const std::string& s );
-
-template< typename T > inline std::string to_rep_string( const T& t ) { return to_string( t ); }
 
 std::string CLASS_BASE_DECL_SPEC int_to_comparable_string( int i, bool prefix_with_sign = true, int max_digits = 0 );
 
