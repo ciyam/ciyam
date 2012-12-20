113c44cb3ca3a97f3f9e6e3f870e4ca15f109a86
Ian Knowles <ian@ciyam.com>
2012-12-20 16:39:43 +0800

    Created 'src' and 'docs' directories and re-organised the repository accordingly.

--- src/init_tests.bat
+++ src/init_tests.bat
@@ -7,6 +7,6 @@ REM in the root project directory or http://www.opensource.org/licenses/mit-lice
 if not exist packages.lst echo Standard>packages.lst
 call construct @packages.lst ciyam_class.cpp.xrep
 
-if exist .git\hooks\pre-commit del .git\hooks\pre-commit
-if exist pre-commit copy pre-commit .git\hooks >nul
+if exist ..\.git\hooks\pre-commit del ..\.git\hooks\pre-commit
+if exist pre-commit copy pre-commit ..\.git\hooks >nul
 
