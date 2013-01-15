864612ec57cc9cff742b7bba1ec177cfe3341664
Ian Knowles <ian@ciyam.com>
2013-01-15 15:27:52 +0800

    Fixed backslash escaping issue.

--- docs/genhtml.bat
+++ docs/genhtml.bat
@@ -2,7 +2,7 @@
 dir /b *.txt >~genhtml.lst
 
 ..\src\xrep @index.html.xrep docs=@~genhtml.lst>index.html
-..\src\xrep @gendocs.xrep prefix=..\src\ docs=@~genhtml.lst>~genhtml.bat
+..\src\xrep @gendocs.xrep prefix=..\\src\\ docs=@~genhtml.lst>~genhtml.bat
 
 call ~genhtml.bat
 
