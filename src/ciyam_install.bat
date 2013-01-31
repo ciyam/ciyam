ecfa91cfdb8595173b99e4d3d5387407263e3bfa
K1773R <K1773R@darkgamex.ch>
2013-01-31 13:06:17 +0100

    changed installscripts so they can be run in the src folder and added copyright header

--- src/ciyam_install.bat
+++ src/ciyam_install.bat
@@ -1,4 +1,8 @@
 @echo off
+REM Copyright (c) 2013 CIYAM Open Developers
+REM Distributed under the MIT/X11 software license, please refer to the file license.txt
+REM in the root project directory or http://www.opensource.org/licenses/mit-license.php.
+REM Task #T00000 - Create installation scripts for Linux and Windows
 
 setlocal
 
@@ -9,11 +13,11 @@ echo Creating %install_path%...
 mkdir "%install_path%"
 
 :check_git
-if exist ".git\config" goto install_copy
+if exist "..\.git\config" goto install_copy
 echo "ERROR: this isnt a ciyam git"
 exit 1
 
 :install_copy
-xcopy src/* "%install_path%" /E /Q /H /R /K /X /Y
+xcopy ..\src\* "%install_path%" /E /Q /H /R /K /X /Y
 
 exit 0
