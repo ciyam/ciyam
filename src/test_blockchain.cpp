00d65d8ecac1ce74ca2c2a16ca124263cdaf44b2
Ian Knowles <ian@ciyam.com>
2014-12-13 14:48:58 +0800

    Added optional tolerance multiplier value for skipping block minting.

--- src/test_blockchain.cpp
+++ src/test_blockchain.cpp
@@ -29,8 +29,11 @@ namespace
 
 struct account
 {
+   account( ) : id( 0 ), skips( 0 ) { }
+
    uint64_t id;
    string seed;
+   unsigned int skips;
    vector< string > secrets;
 };
 
@@ -76,8 +79,8 @@ uint64_t get_account_hit( const string& hash, uint64_t id, size_t height )
       return id - val;
 }
 
-string generate_blockchain_script(
- const string& chain_meta, size_t initial_accounts, size_t rounds, const char* p_seed = 0 )
+string generate_blockchain_script( const string& chain_meta,
+ size_t initial_accounts, size_t rounds, const char* p_seed = 0, int tolerance_multiplier = 0 )
 {
    string seed;
    bool had_fixed_seed = false;
@@ -99,6 +102,11 @@ string generate_blockchain_script(
    string script( "file_raw -core -full blob " );
 
    string header( "blk:a=" + to_string( root_id ) + ",h=0,cm=r:" + chain_meta + ",pk=" + root_pub_key );
+
+   uint64_t initial_tolerance = 0;
+   string::size_type pos = chain_meta.find( '<' );
+   if( pos != string::npos )
+      initial_tolerance = from_string< uint64_t >( chain_meta.substr( pos + 1 ) );
 
    script += header + "\\n";
    string validate( header );
@@ -154,6 +162,10 @@ string generate_blockchain_script(
       uint64_t least_weight = 0;
       string next_last_hash, next_block_hash;
 
+      uint64_t current_tolerance = 0;
+      if( initial_tolerance )
+         current_tolerance = initial_tolerance / num_accounts;
+
       vector< string > new_last_hashes;
       vector< string > new_block_hashes;
       vector< uint64_t > new_total_weights;
@@ -161,6 +173,13 @@ string generate_blockchain_script(
       for( size_t j = 0; j < num_accounts; j++ )
       {
          string previous_block( block_hash );
+
+         // NOTE: If not wanting determinstic data then randomly decide whether to skip.
+         if( !had_fixed_see && rand( ) % num_accounts == j )
+         {
+            ++accounts[ j ].skips;
+            continue;
+         }
 
          // NOTE: If not wanting determinstic data then randomly choose a previous block.
          if( !had_fixed_seed && !block_hashes.empty( ) )
@@ -173,7 +192,17 @@ string generate_blockchain_script(
          }
 
          uint64_t next_weight = get_account_hit( last_hash, accounts[ j ].id, i );
-         string next_hash( base64::encode( get_hash( accounts[ j ].seed, i, rounds ) ) );
+
+         if( tolerance_multiplier && current_tolerance )
+         {
+            if( next_weight > tolerance_multiplier * current_tolerance )
+            {
+               ++accounts[ j ].skips;
+               continue;
+            }
+         }
+
+         string next_hash( base64::encode( get_hash( accounts[ j ].seed, i - accounts[ j ].skips, rounds ) ) );
 
          string data( "blk:a=" + to_string( root_id )
           + "." + to_string( accounts[ j ].id ) + ",h=" + to_string( i ) );
@@ -186,13 +215,13 @@ string generate_blockchain_script(
          validate += next;
          data += next + "\\\n";
 
-         private_key priv_key( accounts[ j ].secrets[ i ] );
+         private_key priv_key( accounts[ j ].secrets[ i - accounts[ j ].skips ] );
          next = ",al=" + priv_key.get_address( true, true ) + ",pb=" + previous_block;
 
          data += next;
          validate += next;
 
-         private_key sign_key( accounts[ j ].secrets[ i - 1 ] );
+         private_key sign_key( accounts[ j ].secrets[ i - accounts[ j ].skips - 1 ] );
 
          next = ",pk=" + sign_key.get_public( true, true ) + ",tw=" + to_string( total_weight + next_weight );
 
@@ -239,7 +268,7 @@ int main( int argc, char* argv[ ] )
    {
       if( argc < 4 )
       {
-         cout << "usage: test_blockchain <chain_meta> <num_accounts> <num_rounds> [<seed>]\n";
+         cout << "usage: test_blockchain <chain_meta> <num_accounts> <num_rounds> [<seed> [<tolerance multiplier>]]\n";
          return 0;
       }
       else
@@ -252,13 +281,18 @@ int main( int argc, char* argv[ ] )
          if( argc > 4 )
             seed = string( argv[ 4 ] );
 
+         int tolerance_multiplier = 0;
+         if( argc > 5 )
+            tolerance_multiplier = atoi( argv[ 5 ] );
+
          if( seed.length( ) && seed.length( ) < 8 )
             throw runtime_error( "seed must be at least 8 bytes" );
 
          if( num_accounts < 1 || num_accounts > 100 )
             throw runtime_error( "num accounts must be in the range 1..100" );
 
-         cout << generate_blockchain_script( chain_meta, num_accounts, num_rounds, seed.empty( ) ? "" : seed.c_str( ) ) << endl;
+         cout << generate_blockchain_script( chain_meta, num_accounts,
+          num_rounds, seed.empty( ) ? "" : seed.c_str( ), tolerance_multiplier ) << endl;
       }
    }
    catch( exception& x )
