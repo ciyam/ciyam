4dca9659b59aded254bfdfc87bac3f71d9513f42
ciyam <ian@ciyam.org>
2017-01-14 13:04:46 +0800

    Added 'restore' batch file for Windows.

---
+++ src/restore.bat
@@ -0,0 +1,19 @@
+@echo off
+REM Copyright (c) 2017 CIYAM Developers
+REM
+REM Distributed under the MIT/X11 software license, please refer to the file license.txt
+REM in the root project directory or http://www.opensource.org/licenses/mit-license.php.
+
+setlocal
+if '%1' == '' goto usage
+
+set CIYAM_STORAGE=%1
+ciyam_client -quiet -no_prompt < restore.cin
+goto end
+
+:usage
+echo Usage: restore [app name]
+
+:end
+endlocal
+
