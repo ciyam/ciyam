724cdc484b81703c3c118e8547ea1261e4e4e89d
ciyam <ian@ciyam.org>
2017-01-24 19:16:58 +0800

    Added 'litecoin' and 'tlitecoin' batch files along with other minor enhancements.

--- src/reconstruct_meta.bat
+++ src/reconstruct_meta.bat
@@ -12,6 +12,7 @@ construct @packages.lst ciyam_class.cpp.xrep
 :skip
 call destroy_apps_and_models.bat %1
 call reinstall_packages.bat %1
+call install.bat Meta meta
 
 fd autoscript.sio.default autoscript.sio >nul
 if errorlevel 1 goto auto
---
+++ src/tlitecoin.bat
@@ -0,0 +1,23 @@
+@echo off
+REM Copyright (c) 2014-2017 CIYAM Developers
+REM
+REM Distributed under the MIT/X11 software license, please refer to the file license.txt
+REM in the root project directory or http://www.opensource.org/licenses/mit-license.php.
+
+setlocal
+
+if not '%TLITECOIN%' == '' goto next
+SET TLITECOIN="%ProgramFiles%\litecoin\litecoin-cli.exe" -regtest -rpcpassword=not_what_you_think_it_is
+
+:next
+if '%1' == '' goto usage
+
+%TLITECOIN% %*
+goto end
+
+:usage
+echo Usage: tlitecoin [command] [[arg1 [arg2 [...]]]
+
+:end
+endlocal
+
