434c8394f4ebfdba69ea50e5512a61655f1be4ca
Ian Knowles <1311212+ciyam@users.noreply.github.com>
2022-07-17 21:36:52 +1000

    Increased peer and file transfer timeout values by five seconds and added calls to 'tree_touch' and 'tree_insert_branch' to 'backup_import'.

--- src/peer_session.cpp
+++ src/peer_session.cpp
@@ -109,7 +109,7 @@ const size_t c_wait_sleep_time = 250;
 const size_t c_start_sleep_time = 2500;
 
 const size_t c_initial_timeout = 25000;
-const size_t c_request_timeout = 15000;
+const size_t c_request_timeout = 20000;
 const size_t c_support_timeout = 10000;
 
 const size_t c_main_session_sleep_time = 150;
