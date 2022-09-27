f264cb75ce57be830f91c07ece3ee1c74a8a53ad
Ian Knowles <1311212+ciyam@users.noreply.github.com>
2022-09-27 18:38:18 +1000

    Changed blockchain generation to create a blockchain archive (along with a system variable for its path) and changed various tree trunk batch/scripts to support a comma separated list of trunks.

--- src/tree_remove_trunks.bat
+++ src/tree_remove_trunks.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Copyright (c) 2021-2022 CIYAM Developers
+REM Copyright (c) 2022 CIYAM Developers
 REM
 REM Distributed under the MIT/X11 software license, please refer to the file license.txt
 REM in the root project directory or http://www.opensource.org/licenses/mit-license.php.
@@ -7,13 +7,13 @@ REM in the root project directory or http://www.opensource.org/licenses/mit-lice
 if '%1' == '' goto usage
 if '%2' == '' goto usage
 
-echo ^<^<tree_insert_trunk.cin %1 %2 >~tree_insert_trunk
-ciyam_client -echo -quiet -no_prompt -no_stderr < ~tree_insert_trunk
+echo ^<^<tree_remove_trunks.cin %1 %2 >~tree_remove_trunks
+ciyam_client -echo -quiet -no_prompt -no_stderr < ~tree_remove_trunks
 
-del ~tree_insert_trunk
+del ~tree_remove_trunks
 goto end
 
 :usage
-echo Usage: tree_insert_trunk [tree tag] [trunk tag]
+echo Usage: tree_remove_trunks [tree tag] [trunk item(s)]
 
 :end
