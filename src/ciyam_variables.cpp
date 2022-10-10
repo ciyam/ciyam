34de4b03ad668416570702bab1c981ddad9f4e0c
Ian Knowles <1311212+ciyam@users.noreply.github.com>
2022-10-10 15:51:18 +1100

    Fixed issue with reading from 'Import.txt' for 'Import Notes' Peerchain field and incorrect system variable usage for backup/shared height.

--- src/ciyam_variables.cpp
+++ src/ciyam_variables.cpp
@@ -163,7 +163,9 @@ const char* const c_special_variable_blockchain_zenith_hash = "@blockchain_zenit
 const char* const c_special_variable_blockchain_archive_path = "@blockchain_archive_path";
 const char* const c_special_variable_blockchain_backup_ident = "@blockchain_backup_ident";
 const char* const c_special_variable_blockchain_shared_ident = "@blockchain_shared_ident";
+const char* const c_special_variable_blockchain_backup_height = "@blockchain_backup_height";
 const char* const c_special_variable_blockchain_peer_is_owner = "@blockchain_peer_is_owner";
+const char* const c_special_variable_blockchain_shared_height = "@blockchain_shared_height";
 const char* const c_special_variable_blockchain_zenith_height = "@blockchain_zenith_height";
 const char* const c_special_variable_blockchain_get_tree_files = "@blockchain_get_tree_files";
 const char* const c_special_variable_blockchain_num_tree_items = "@blockchain_num_tree_items";
@@ -683,8 +685,16 @@ string get_special_var_name( special_var var )
       s = string( c_special_variable_blockchain_shared_ident );
       break;
 
+      case e_special_var_blockchain_backup_height:
+      s = string( c_special_variable_blockchain_backup_height );
+      break;
+
       case e_special_var_blockchain_peer_is_owner:
       s = string( c_special_variable_blockchain_peer_is_owner );
+      break;
+
+      case e_special_var_blockchain_shared_height:
+      s = string( c_special_variable_blockchain_shared_height );
       break;
 
       case e_special_var_blockchain_zenith_height:
