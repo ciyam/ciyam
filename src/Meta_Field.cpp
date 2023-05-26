22ae9a194a1b2de1580542644504542cf0e273e6
ciyam <1311212+ciyam@users.noreply.github.com>
2023-05-26 23:33:28 +1000

    Added 'open_channel' and 'close_channel' application protocol scripts and added 'key_name' enumeration to Meta Field Extra.

--- src/Meta_Field.cpp
+++ src/Meta_Field.cpp
@@ -616,6 +616,7 @@ const int c_enum_field_extra_qr_code( 29 );
 const int c_enum_field_extra_filename( 22 );
 const int c_enum_field_extra_field_hash( 25 );
 const int c_enum_field_extra_password( 10 );
+const int c_enum_field_extra_key_name( 34 );
 const int c_enum_field_extra_defcurrent( 11 );
 const int c_enum_field_extra_permission( 17 );
 const int c_enum_field_extra_orientation( 24 );
@@ -679,6 +680,8 @@ string get_enum_string_field_extra( int val )
       string_name = "enum_field_extra_field_hash";
    else if( to_string( val ) == to_string( "10" ) )
       string_name = "enum_field_extra_password";
+   else if( to_string( val ) == to_string( "34" ) )
+      string_name = "enum_field_extra_key_name";
    else if( to_string( val ) == to_string( "11" ) )
       string_name = "enum_field_extra_defcurrent";
    else if( to_string( val ) == to_string( "17" ) )
@@ -8385,6 +8388,7 @@ void Meta_Field::static_get_all_enum_pairs( vector< pair< string, string > >& pa
    pairs.push_back( make_pair( "enum_field_extra_22", get_enum_string_field_extra( 22 ) ) );
    pairs.push_back( make_pair( "enum_field_extra_25", get_enum_string_field_extra( 25 ) ) );
    pairs.push_back( make_pair( "enum_field_extra_10", get_enum_string_field_extra( 10 ) ) );
+   pairs.push_back( make_pair( "enum_field_extra_34", get_enum_string_field_extra( 34 ) ) );
    pairs.push_back( make_pair( "enum_field_extra_11", get_enum_string_field_extra( 11 ) ) );
    pairs.push_back( make_pair( "enum_field_extra_17", get_enum_string_field_extra( 17 ) ) );
    pairs.push_back( make_pair( "enum_field_extra_24", get_enum_string_field_extra( 24 ) ) );
@@ -8516,6 +8520,7 @@ void Meta_Field::static_class_init( const char* p_module_name )
    g_field_extra_enum.insert( 22 );
    g_field_extra_enum.insert( 25 );
    g_field_extra_enum.insert( 10 );
+   g_field_extra_enum.insert( 34 );
    g_field_extra_enum.insert( 11 );
    g_field_extra_enum.insert( 17 );
    g_field_extra_enum.insert( 24 );
