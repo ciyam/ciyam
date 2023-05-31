a97d382a122cc2a91f711a71e6f406b52f867f94
ciyam <1311212+ciyam@users.noreply.github.com>
2023-05-31 15:32:49 +1000

    Added new '[finishing]' value for the '-term' usage of the application protocol 'notifier' command and related changes.

--- src/ciyam_constants.h
+++ src/ciyam_constants.h
@@ -28,7 +28,8 @@ const char* const c_false_value = "0";
 
 const char* const c_dummy = "dummy";
 
-const char* const c_watch_str = "[watch]";
+const char* const c_watching = "[watching]";
+const char* const c_finishing = "[finishing]";
 
 const char* const c_ciyam_tag = "ciyam";
 const char* const c_ciyam_logs_tag = "ciyam.logs";
