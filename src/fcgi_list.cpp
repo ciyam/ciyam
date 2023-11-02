e1127ecab0148cbc16a0c5adc36b8610a4869e75
ciyam <1311212+ciyam@users.noreply.github.com>
2023-11-02 15:57:13 +1100

    Added a trailing '/' for the 'sftp' hyperlink (and changed the FCGI UI accordingly) in order to behave better when using WinSCP.

--- src/fcgi_list.cpp
+++ src/fcgi_list.cpp
@@ -3328,17 +3328,20 @@ void output_list_form( ostream& os,
                else if( source.mailto_fields.count( source_value_id ) )
                   type = "mailto:";
 
-               string::size_type pos = cell_data.rfind( '/' );
+               string display( cell_data );
 
-               if( ( pos == string::npos ) || ( pos == cell_data.size( ) - 1 ) )
-                  pos = 0;
-               else
-                  ++pos;
+               if( !display.empty( ) && display[ display.length( ) - 1 ] == '/' )
+                  display.erase( display.length( ) - 1 );
+
+               string::size_type pos = display.rfind( '/' );
+
+               if( pos != string::npos )
+                  display.erase( 0, pos + 1 );
 
                is_href = true;
 
                os << "<a href=\"" << type << cell_data << "\"" << extra << ">"
-                << data_or_nbsp( escape_markup( unescaped( cell_data.substr( pos ) ) ) ) << "</a>";
+                << data_or_nbsp( escape_markup( unescaped( display ) ) ) << "</a>";
             }
 
             if( !cell_data.empty( )
