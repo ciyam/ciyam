351b2aa9f1bcac9dcf9d9f4f2ab8bb315f939248
ciyam <1311212+ciyam@users.noreply.github.com>
2024-01-15 15:37:04 +1300

    Added code to default the stream cipher type for interactive (i.e. non-blockchain) peer session testing.

--- src/peer_session.cpp
+++ src/peer_session.cpp
@@ -908,7 +908,12 @@ void process_repository_file( const string& blockchain,
           get_special_var_name( e_special_var_blockchain_stream_cipher ) ) );
 
          if( stream_cipher.empty( ) )
-            throw runtime_error( "unexpected missing @stream_cipher in process_repository_file" );
+         {
+            if( is_test_session )
+               stream_cipher = stream_cipher_string( );
+            else
+               throw runtime_error( "unexpected missing @stream_cipher in process_repository_file" );
+         }
 
          stringstream ss( file_content );
 
