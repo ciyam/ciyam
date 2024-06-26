c127594f2a91a71f2114c3ca3b622cad194dcf3b
ciyam <1311212+ciyam@users.noreply.github.com>
2024-06-26 21:40:51 +1200

    Added 'locked.sh' bash script along with supporting code and removed 'lockfile' usage (and file existence checks) - also added quotes around '~' usage in various bash scripts to avoid possible shell expansion.

---
+++ src/locked.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Copyright (c) 2024 CIYAM Developers
+#
+# Distributed under the MIT/X11 software license, please refer to the file license.txt
+# in the root project directory or http://www.opensource.org/licenses/mit-license.php.
+
+LOCK_NAME=$1
+
+exit_code=0
+
+echo -n $$ >>"$LOCK_NAME"
+
+cleanup( )
+{
+ rm -f "$LOCK_NAME"
+ exit $exit_code
+}
+
+trap cleanup EXIT ERR INT TERM
+
+shift
+$*
+
+rm -f "$LOCK_NAME"
+
+exit_code=$?
---
+++ src/test4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Copyright (c) 2024 CIYAM Developers
+#
+# Distributed under the MIT/X11 software license, please refer to the file license.txt
+# in the root project directory or http://www.opensource.org/licenses/mit-license.php.
+
+echo "(started test4)" >>"~test.txt"
+echo "(finished test4)" >>"~test.txt"
