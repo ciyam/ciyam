590c08c0c0c9e53bc835dcdfa9124d71aaf4fc3e
ciyam <1311212+ciyam@users.noreply.github.com>
2025-08-02 22:40:48 +1200

    Added index for Procedure needed for the ODS local storage implementation of Meta.

--- src/Meta_Procedure.cpp
+++ src/Meta_Procedure.cpp
@@ -2765,6 +2765,7 @@ void Meta_Procedure::static_get_all_index_pairs( vector< pair< string, string > 
 {
    pairs.push_back( make_pair( "Class,Id", "string,string" ) );
    pairs.push_back( make_pair( "Class,Name", "string,string" ) );
+   pairs.push_back( make_pair( "Source_Procedure,@pk", "string,string" ) );
 }
 
 void Meta_Procedure::static_get_all_unique_indexes( vector< string >& unique_indexes )
@@ -2777,6 +2778,7 @@ void Meta_Procedure::static_get_sql_indexes( vector< string >& indexes )
 {
    indexes.push_back( "C_Class,C_Id" );
    indexes.push_back( "C_Class,C_Name" );
+   indexes.push_back( "C_Source_Procedure,C_Key_" );
 }
 
 void Meta_Procedure::static_get_sql_unique_indexes( vector< string >& indexes )
