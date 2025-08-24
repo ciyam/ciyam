1a0a2c13f0d19b3c3d14fd1f0c06b5ae5f4b97fb
ciyam <1311212+ciyam@users.noreply.github.com>
2025-08-24 14:19:46 +1200

    Removed trailing space from the application UUID in the Generate procedure.

--- src/Meta_Application.cpp
+++ src/Meta_Application.cpp
@@ -2360,7 +2360,7 @@ void Meta_Application::impl::impl_Generate( )
          }
 
          outs << " if [ ! -f " << get_obj( ).Name( ) << ".log ]; then\n";
-         outs << "  echo \"[0]" << uuid( ).as_string( ) << " \" > " << get_obj( ).Name( ) << ".log\n";
+         outs << "  echo \"[0]" << uuid( ).as_string( ) << "\" > " << get_obj( ).Name( ) << ".log\n";
          outs << "  echo \"[1];module ==> none\" >> " << get_obj( ).Name( ) << ".log\n";
          outs << "  echo \"[2];dummy init\" >> " << get_obj( ).Name( ) << ".log\n";
          outs << " fi\n";
