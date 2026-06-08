1fcf90c9abafa8cda1b24ef34bb9b045fc6beb97
ciyam <1311212+ciyam@users.noreply.github.com>
2026-06-08 22:24:36 +1200

    Added new basic server regression tests for endpoints and file uploading.

---
+++ tests/test_server_3_a.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Simple API tests.
+curl -s "localhost:13031/ip_addr"
+curl -s "localhost:13031/version"
+curl -s -4 "localhost:13031/ip_addr?format=text"
+curl -s "localhost:13031/ip_addr?format=json"
+curl -s "localhost:13031/version?format=json"
+curl -s -4 "localhost:13031/ip_addr?format=json"
+curl -s "localhost:13031/version?format=none"
+curl -s "localhost:13031/version?format=text&extra="
+curl -s -d "testing..." "localhost:13031/echo"
+curl -s -d "testing..." "localhost:13031/echo?format=json"
+
+# Basic file upload API tests.
+filename=$(curl -s -F "file=@test.jpg" localhost:13031/upload)
+diff test.jpg $filename
+rm -f $filename
+filename=$(curl -s -F "file=@test.png" localhost:13031/upload)
+diff test.png $filename
+rm -f $filename
+
+# Basic web session API tests.
+echo -n "pwd_hash" > .web_session.test
+unique=$(curl -s "localhost:13031/cws?token=test&device=dummy")
+session=$(echo -n "pwd_hash$unique" | sha256sum | head -c 10)
+curl -s "localhost:13031/cws?token=test&device=dummy&session=$session"
+curl -s "localhost:13031/cws?token=test&device=dummy&session=bad_$session"
+curl -s "localhost:13031/cws?token=test&device=dummy&session=$session"
+curl -s "localhost:13031/cws?token=test&device=dummy&session=$session&function=terminate"
+curl -s "localhost:13031/cws?token=test&device=dummy&session=$session"
+rm -f .web_session.test
---
+++ tests/test_server_3_a.tst
@@ -0,0 +1,15 @@
+::1
+0.0.0
+127.0.0.1
+{"ip_addr":"::1"}
+{"version":"0.0.0"}
+{"ip_addr":"127.0.0.1"}
+Invalid query parameter value in 'format=none' for version.
+0.0.0
+testing...
+{"data":"testing..."}
+[running]
+This web session is not valid (or has expired).
+[running]
+Session terminated.
+This web session is not valid (or has expired).
