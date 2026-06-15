55b72970efe767214b731448b0bc99b5dd28248e
ciyam <1311212+ciyam@users.noreply.github.com>
2026-06-15 15:13:30 +1200

    Updated CWS API regression tests to use 'access' rather than 'token' and recaptured test output.

--- tests/test_server_3_a.sh
+++ tests/test_server_3_a.sh
@@ -7,7 +7,8 @@ curl -s -4 "localhost:13031/api.ip_addr?format=text"
 curl -s "localhost:13031/api.ip_addr?format=json"
 curl -s "localhost:13031/api.version?format=json"
 curl -s -4 "localhost:13031/api.ip_addr?format=json"
-curl -s "localhost:13031/api.version?format=none"
+curl -s "localhost:13031/api.version"
+curl -s "localhost:13031/api.version?format=xxxx"
 curl -s "localhost:13031/api.version?format=text&extra="
 curl -s "localhost:13031/api.post_limit"
 curl -s "localhost:13031/api.post_limit?format=json"
@@ -25,14 +26,14 @@ rm -f $filename
 
 # Basic web session API tests.
 echo -n "pwd_hash_value" > .web_session.test
-device=$(curl -s "localhost:13031/api.cws?token=test")
-unique=$(curl -s "localhost:13031/api.cws?token=test&device=$device")
+device=$(curl -s "localhost:13031/api.cws?access=test")
+unique=$(curl -s "localhost:13031/api.cws?access=test&device=$device")
 checked=$(echo -n "pwd_hash_value$device" | sha256sum | head -c 64)
 session=$(echo -n "$checked$unique" | sha256sum | head -c 10)
-curl -s "localhost:13031/api.cws?token=test&device=bad_device&session=$session"
-curl -s "localhost:13031/api.cws?token=test&device=$device&session=$session"
-curl -s "localhost:13031/api.cws?token=test&device=$device&session=bad_session"
-curl -s "localhost:13031/api.cws?token=test&device=$device&session=$session"
-curl -s "localhost:13031/api.cws?token=test&device=$device&session=$session&function=terminate"
-curl -s "localhost:13031/api.cws?token=test&device=$device&session=$session"
+curl -s "localhost:13031/api.cws?access=test&device=bad_device&session=$session"
+curl -s "localhost:13031/api.cws?access=test&device=$device&session=$session"
+curl -s "localhost:13031/api.cws?access=test&device=$device&session=bad_session"
+curl -s "localhost:13031/api.cws?access=test&device=$device&session=$session"
+curl -s "localhost:13031/api.cws?access=test&device=$device&session=$session&function=terminate"
+curl -s "localhost:13031/api.cws?access=test&device=$device&session=$session"
 rm -f .web_session.test
--- tests/test_server_3_a.tst
+++ tests/test_server_3_a.tst
@@ -1,17 +1,10 @@
 ::1
 0.0.0
-127.0.0.1
-{"ip_addr":"::1"}
-{"version":"0.0.0"}
-{"ip_addr":"127.0.0.1"}
-Error: Invalid query parameter value in 'format=none' for version.
-0.0.0
-100000000
-{"post_limit":"100000000"}
-{"post_limit":"100.0 MB"}
-testing...
-{"data":"testing..."}
-Error: Invalid device identity 'bad_device'.
+127.0.0.1{"ip_addr":"::1"}{"version":"0.0.0"}{"ip_addr":"127.0.0.1"}0.0.0
+Error: Invalid format value 'xxxx'.
+0.0.0100000000
+{"post_limit":"100000000"}{"post_limit":"100.0 MB"}testing...
+{"data":"testing..."}Error: Invalid device identity 'bad_device'.
 [running]
 Error: This web session is not valid (or has expired).
 [running]
