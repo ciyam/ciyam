977036308ce5086a8a1f0b1fdf104f9c8db384cb
ciyam <1311212+ciyam@users.noreply.github.com>
2026-06-21 20:59:17 +1200

    Changed the 'test' web session token file content to be a valid SHA256 digest.

--- tests/test_server_3_a.sh
+++ tests/test_server_3_a.sh
@@ -25,10 +25,10 @@ diff test.png $filename
 rm -f $filename
 
 # Basic web session API tests.
-echo -n "pwd_hash_value" > .web_session.test
+echo -n "f2ca1bb6c7e907d06dafe4687e579fce76b37e4e93b7605022da52e6ccc26fd2" > .web_session.test
 device=$(curl -s "localhost:13031/api.cws?access=test")
 unique=$(curl -s "localhost:13031/api.cws?access=test&device=$device")
-checked=$(echo -n "pwd_hash_value$device" | sha256sum | head -c 64)
+checked=$(echo -n "f2ca1bb6c7e907d06dafe4687e579fce76b37e4e93b7605022da52e6ccc26fd2$device" | sha256sum | head -c 64)
 session=$(echo -n "$checked$unique" | sha256sum | head -c 10)
 curl -s "localhost:13031/api.cws?access=test&device=bad_device&session=$session"
 curl -s "localhost:13031/api.cws?access=test&device=$device&session=$session"
