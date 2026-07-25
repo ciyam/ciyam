bdb02cea65f0d5c9ff98eba391f3e547ce280a31
ciyam <1311212+ciyam@users.noreply.github.com>
2026-07-25 13:47:49 +1200

    Added usage hints to the 'template' sample CWS javascript file.

--- webui/ciyam_script_template.js
+++ webui/ciyam_script_template.js
@@ -1,13 +1,29 @@
 var ciyam_script_template_result = null;
 
+var ciyam_script_template_has_shown_hints = false;
+
 async function ciyam_script_template_at_load( callback )
 {
    console.log( "ciyam_script_template_at_load" );
 
-   ciyam_script_template_result = "ciyam_script_template loaded";
-   ciyam_script_template_result = ciyam_script_template_result.substring( String( "ciyam_script_" ).length );
+   ciyam_script_template_result = "(ciyam_script_template loaded)";
 
-   callback( ciyam_script_template_result );
+   ciyam_script_template_result = ciyam_script_template_result.replace( "ciyam_script_", "" );
+
+   var output = ciyam_script_template_result;
+
+   if( include_script_usage_hints )
+   {
+      var extra = output;
+
+      extra = extra.substr( 1, extra.length - 2 );
+
+      extra = extra.replace( "loaded", "testing" );
+
+      output += "\nemploy javascript " + extra;
+   }
+
+   callback( output );
 }
 
 async function ciyam_script_template_execute( callback, input )
@@ -16,5 +32,14 @@ async function ciyam_script_template_execute( callback, input )
 
    ciyam_script_template_result = input;
 
-   callback( ciyam_script_template_result );
+   var output = ciyam_script_template_result;
+
+   if( include_script_usage_hints && !ciyam_script_template_has_shown_hints )
+   {
+      ciyam_script_template_has_shown_hints = true;
+
+      output += "\n(i.e. will echo the provided argument)"
+   }
+
+   callback( output );
 }
