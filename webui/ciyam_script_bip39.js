e41f0d00be207e0172b1f1952795cb2c115c061f
ciyam <1311212+ciyam@users.noreply.github.com>
2026-07-26 23:02:37 +1200

    Added 'ciyam_script' for using basic BIP39 functionality (including validation and converting between mnemonics and hex entropy).

---
+++ webui/ciyam_script_bip39.js
@@ -0,0 +1,43 @@
+var ciyam_script_bip39_result = null;
+
+var ciyam_script_bip39_has_shown_hints = false;
+
+async function ciyam_script_bip39_at_load( callback )
+{
+   console.log( "ciyam_script_bip39_at_load" );
+
+   ciyam_script_bip39_result = BIP39.generateMnemonic( );
+
+   var output = ciyam_script_bip39_result;
+
+   if( include_script_usage_hints )
+      output += "\nemploy javascript bip39 @output_1";
+
+   callback( output );
+}
+
+async function ciyam_script_bip39_execute( callback, input )
+{
+   console.log( "ciyam_script_bip39_execute" );
+
+   ciyam_script_bip39_result = input;
+
+   if( input == "" )
+      ciyam_script_bip39_result = BIP39.generateMnemonic( );
+   else
+   {
+      var pos = input.indexOf( " " );
+
+      if( pos > 0 )
+      {
+         if( !BIP39.validateMnemonic( input ) )
+            ciyam_script_bip39_result = input + "\n(incorrect mnemonics)";
+         else
+            ciyam_script_bip39_result = BIP39.mnemonicToEntropy( input );
+      }
+      else
+         ciyam_script_bip39_result = BIP39.entropyToMnemonic( input );
+   }
+
+   callback( ciyam_script_bip39_result );
+}
