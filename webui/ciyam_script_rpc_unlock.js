ae540db78bf691b0e268552fd58f1487fcc27ca2
ciyam <1311212+ciyam@users.noreply.github.com>
2026-08-08 21:29:37 +1200

    Added new 'ciyam_script_rpc_unlock' javascript function for being able to create a usable RPC unlock salted and hashed password.

---
+++ webui/ciyam_script_rpc_unlock.js
@@ -0,0 +1,52 @@
+var ciyam_script_rpc_unlock_result = null;
+
+var ciyam_script_rpc_unlock_has_shown_hints = false;
+
+async function ciyam_script_rpc_unlock_at_load( callback )
+{
+   console.log( "ciyam_script_rpc_unlock_at_load" );
+
+   ciyam_script_rpc_unlock_result = "(ciyam_script_rpc_unlock loaded)";
+
+   ciyam_script_rpc_unlock_result = ciyam_script_rpc_unlock_result.replace( "ciyam_script_", "" );
+
+   var output = ciyam_script_rpc_unlock_result;
+
+   if( include_script_usage_hints )
+   {
+      var extra = output;
+
+      extra = extra.substr( 1, extra.length - 2 );
+
+      extra = extra.replace( "loaded", "testing" );
+
+      output += "\nemploy javascript " + extra;
+   }
+
+   callback( output );
+}
+
+async function ciyam_script_rpc_unlock_execute( callback, input )
+{
+   console.log( "ciyam_script_rpc_unlock_execute" );
+
+   var unix_now = Math.floor( Date.now( ) / 1000 );
+   var password_hash = hex_sha256( input ).toLowerCase( );
+   var salted_password_hash = hex_sha256( unix_now + password_hash );
+
+   var output = unix_now + "-" + salted_password_hash;
+
+   if( document.getElementById( "test_image" ).innerHTML != null )
+      document.getElementById( "test_image" ).innerHTML = "";
+
+   new QRCode( document.getElementById( "test_image" ), output );
+
+   if( include_script_usage_hints && !ciyam_script_rpc_unlock_has_shown_hints )
+   {
+      ciyam_script_rpc_unlock_has_shown_hints = true;
+
+      output += "\n./ciyam_client -tls -rpc_unlock=" + output + " localhost:6666";
+   }
+
+   callback( output );
+}
