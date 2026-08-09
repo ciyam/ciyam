29f7d6dcf7cfa5170c7c586170992ed5793df1d2
ciyam <1311212+ciyam@users.noreply.github.com>
2026-08-09 20:45:03 +1200

    Added new 'ciyam_script' for performing XOR on the expected hex input and prior result.

---
+++ webui/ciyam_script_xor_hex_data.js
@@ -0,0 +1,92 @@
+var ciyam_script_xor_hex_data_result = null;
+
+var ciyam_script_xor_hex_data_has_shown_hints = false;
+
+function ciyam_script_xor_hex_data_xor_next( input )
+{
+   var hex_result = '';
+
+   var hex_current = ciyam_script_xor_hex_data_result;
+   var hex_implant = input;
+
+   if( ( hex_current == null ) || ( hex_current == "" ) )
+      hex_current = "00";
+   else if( ( hex_current.length % 2 ) != 0  )
+      hex_current += "0";
+
+   if( ( hex_implant == null ) || ( hex_implant == "" ) )
+      hex_implant = "00";
+   else if( ( hex_implant.length % 2 ) != 0  )
+      hex_implant += "0";
+
+   if( hex_current.length < hex_implant.length )
+      hex_current = hex_current.padStart( hex_implant.length, "0" );
+   else if( hex_implant.length < hex_current.length )
+      hex_implant = hex_implant.padStart( hex_current.length, "0" );
+
+   for( i = 0; i < hex_current.length; i += 2 )
+   {
+      var new_hex = hex_implant.charAt( i );
+      new_hex += hex_implant.charAt( i + 1 );
+
+      var new_val = parseInt( new_hex, 16 );
+
+      var old_hex = hex_current.charAt( i );
+      old_hex += hex_current.charAt( i + 1 );
+
+      var old_val = parseInt( old_hex, 16 );
+
+      var new_val = ( old_val ^ new_val );
+
+      if( new_val < 16 )
+         hex_result += '0';
+
+      hex_result += new_val.toString( 16 );
+   }
+
+   return hex_result;
+}
+
+async function ciyam_script_xor_hex_data_at_load( callback )
+{
+   console.log( "ciyam_script_xor_hex_data_at_load" );
+
+   ciyam_script_xor_hex_data_result = "(ciyam_script_xor_hex_data loaded)";
+
+   ciyam_script_xor_hex_data_result = ciyam_script_xor_hex_data_result.replace( "ciyam_script_", "" );
+
+   var output = ciyam_script_xor_hex_data_result;
+
+   if( include_script_usage_hints )
+   {
+      var extra = output;
+
+      extra = extra.substr( 1, extra.length - 2 );
+
+      extra = extra.replace( "loaded", "01020304" );
+
+      output += "\nemploy javascript " + extra;
+   }
+
+   ciyam_script_xor_hex_data_result = null;
+
+   callback( output );
+}
+
+async function ciyam_script_xor_hex_data_execute( callback, input )
+{
+   console.log( "ciyam_script_xor_hex_data_execute" );
+
+   var output = ciyam_script_xor_hex_data_xor_next( input );
+
+   ciyam_script_xor_hex_data_result = output;
+
+   if( include_script_usage_hints && !ciyam_script_xor_hex_data_has_shown_hints )
+   {
+      ciyam_script_xor_hex_data_has_shown_hints = true;
+
+      output += "\n(i.e. XORs the prior result with hex input)"
+   }
+
+   callback( output );
+}
