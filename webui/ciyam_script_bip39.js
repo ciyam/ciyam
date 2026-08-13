3240ec6c708062c2b2a4c786f732348ea6ace854
ciyam <1311212+ciyam@users.noreply.github.com>
2026-08-13 15:36:24 +1200

    Changed the 'at_load' function to call 'execute' if an 'init_script_value' was provided and added validation for 'entropy' input.

--- webui/ciyam_script_bip39.js
+++ webui/ciyam_script_bip39.js
@@ -6,19 +6,33 @@ async function ciyam_script_bip39_at_load( callback )
 {
    console.log( "ciyam_script_bip39_at_load" );
 
-   ciyam_script_bip39_result = BIP39.generateMnemonic( );
+   if( init_script_value != null )
+   {
+      ciyam_script_bip39_result = null;
 
-   var output = ciyam_script_bip39_result;
+      ciyam_script_bip39_execute( callback, init_script_value );
+   }
+   else
+   {
+      ciyam_script_bip39_result = BIP39.generateMnemonic( );
 
-   if( include_script_usage_hints )
-      output += "\nemploy javascript bip39 {@1}";
+      var output = ciyam_script_bip39_result;
 
-   callback( output );
+      if( include_script_usage_hints )
+      {
+         ciyam_script_bip39_has_shown_hints = true;
+
+         output += "\nemploy javascript bip39 {@1}";
+      }
+
+      callback( output );
+   }
 }
 
 async function ciyam_script_bip39_execute( callback, input )
 {
-   console.log( "ciyam_script_bip39_execute" );
+   if( ciyam_script_bip39_result != null )
+      console.log( "ciyam_script_bip39_execute" );
 
    ciyam_script_bip39_result = input;
 
@@ -36,7 +50,33 @@ async function ciyam_script_bip39_execute( callback, input )
             ciyam_script_bip39_result = BIP39.mnemonicToEntropy( input );
       }
       else
-         ciyam_script_bip39_result = BIP39.entropyToMnemonic( input );
+      {
+         var is_valid_entropy = true;
+
+         if( input.length != 32 )
+            is_valid_entropy = false;
+         else
+         {
+            for( i = 0; i < input.length; i++ )
+            {
+               var next = input.substr( i, 1 ).toLowerCase( );
+
+               if( ( next < 0 ) || ( next > 9 ) )
+               {
+                  if( ( next < 'a' ) || ( next > 'f' ) )
+                  {
+                     is_valid_entropy = false;
+                     break;
+                  }
+               }
+            }
+         }
+
+         if( !is_valid_entropy )
+            ciyam_script_bip39_result = input + "\n(invalid entropy)";
+         else
+            ciyam_script_bip39_result = BIP39.entropyToMnemonic( input );
+      }
    }
 
    callback( ciyam_script_bip39_result );
