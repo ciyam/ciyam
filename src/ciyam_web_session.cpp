cf38b69bd1644e79299cd6367a102529689bbc78
ciyam <1311212+ciyam@users.noreply.github.com>
2026-09-07 20:04:06 +1200

    Increased the number of iterations when waiting for output but reduced the sleep time by half for the first ten iterations.

--- src/ciyam_web_session.cpp
+++ src/ciyam_web_session.cpp
@@ -2816,7 +2816,7 @@ bool process_cws_request( http_request_type request_type, const string& uri_suff
                               set_system_variable( web_command_var_name, "variable " + web_message_var_name );
                            }
 
-                           for( size_t i = 0; i < 25; i++ )
+                           for( size_t i = 0; i < 30; i++ )
                            {
                               if( file_exists( output_file_name ) )
                               {
@@ -2839,7 +2839,7 @@ bool process_cws_request( http_request_type request_type, const string& uri_suff
                                  break;
                               }
 
-                              msleep( 100 );
+                              msleep( ( i < 10 ) ? 50 : 100 );
                            }
 
                            if( has_error_prefix( response ) )
